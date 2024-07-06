45078a17abdc0486418e5d817c277921b217493b
Danial <82217321+DanialK007@users.noreply.github.com>
2024-07-06 21:30:52 +0630

    v 1.1

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,18 +1,7 @@
 import React from 'react'
-import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 function Navbar() {
   return (
-    // <div className='fixed top-0 left-0 z-50 w-full py-5'>
-    //     <div className="max-w-screen-xl mx-auto flex items-center justify-between">
-    //         <div className="">DanialK</div>  
-    //         <div className="flex gap-8">
-    //             <FaGithub />
-    //             <FaLinkedin />
-    //             <FaInstagram />
-    //         </div>
-    //     </div>
-    // </div>
     <>
       <div className="fixed top-5 left-0 w-full z-[999]">
           <div className="flex items-center justify-center gap-8 bg-light/5 backdrop-blur-md border border-light/20 w-fit mx-auto p-2">
