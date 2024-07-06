9f4b5cdb2c546164bdfc4fc9452e36f86ef73f14
Danial <82217321+DanialK007@users.noreply.github.com>
2024-07-06 21:39:08 +0630

    v 1.1

--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
     <>
       <div className="fixed top-5 left-0 w-full z-[999]">
           <div className="flex items-center justify-center gap-8 bg-light/5 backdrop-blur-md border border-light/20 w-fit mx-auto p-2">
-            <a href="" className='font-mono text-xl px-2'>Danial.K</a>
+            <a href="" className='font-mono text-xl px-2'>DanialK</a>
             <a href="" className='uppercase hidden lg:block'>Services</a>
             <a href="" className='uppercase hidden lg:block'>Projects</a>
             <a href="" className='uppercase hidden lg:block'>About</a>
